48249043cd57c385783f6f6b6a6e01ce03893808
ddoyediran <ddoyediran@gmail.com>
2022-04-21 17:29:16 +0200

    updates

---
+++ README.md
@@ -0,0 +1,1 @@
+I developed this simple code (project) to extract table from pdf or journal for my graduate school masters thesis.
--- main.py
+++ main.py
@@ -1,20 +1,19 @@
 import tabula
 
-#pdf_path = "https://sedl.org/afterschool/toolkits/science/pdf/ast_sci_data_tables_sample.pdf"
-
-#dfs = tabula.read_pdf(pdf_path, pages=1)
-
 #df = tabula.read_pdf("https://sedl.org/afterschool/toolkits/science/pdf/ast_sci_data_tables_sample.pdf", pages="all")
 
 pdf_path = "tables_sample.pdf"
 
-df = tabula.read_pdf(pdf_path, pages="1")
+df = tabula.read_pdf(pdf_path, pages="2")
 
 #print(df)
 
 # We can use these 2 methods if we are sure we only have one table on a page
 df[0].to_csv("first_tb") # method 1: will create first_tb.csv 
 
-tabula.convert_into(pdf_path, "first_tb_2", output_format="csv", pages="1") # method 2: will create first_tb_2.csv 
+# tabula.convert_into(pdf_path, "first_tb_2", output_format="csv", pages="1") # method 2: will create first_tb_2.csv 
 
-#print("Dami is working")
+# To extract multiple table from a single page
+    # Note this will extract all the tables into a single worksheet
+for i in range(len(df)):
+    df[i].to_csv(f"page_2_tb{i}.csv") 
